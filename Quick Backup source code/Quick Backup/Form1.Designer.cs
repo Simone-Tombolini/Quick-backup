e3dc09c0ee947bb4908355480fc737caa313b46f
Simone-Tombolini <74507728+Simone-Tombolini@users.noreply.github.com>
2022-02-08 01:52:10 +0100

    Add files via upload

---
+++ Quick Backup source code/Quick Backup/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Quick_Backup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBoxDefault = new System.Windows.Forms.CheckBox();
+            this.buttonSearchDestiantion = new System.Windows.Forms.Button();
+            this.buttonSearchSource = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDestination = new System.Windows.Forms.TextBox();
+            this.textBoxSouce = new System.Windows.Forms.TextBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(522, 58);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(71, 21);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Modify";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged_1);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(522, 23);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(71, 21);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Modify";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBoxDefault
+            // 
+            this.checkBoxDefault.AutoSize = true;
+            this.checkBoxDefault.Location = new System.Drawing.Point(15, 101);
+            this.checkBoxDefault.Name = "checkBoxDefault";
+            this.checkBoxDefault.Size = new System.Drawing.Size(153, 21);
+            this.checkBoxDefault.TabIndex = 4;
+            this.checkBoxDefault.Text = "Use as default path\r\n";
+            this.checkBoxDefault.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearchDestiantion
+            // 
+            this.buttonSearchDestiantion.Location = new System.Drawing.Point(447, 55);
+            this.buttonSearchDestiantion.Name = "buttonSearchDestiantion";
+            this.buttonSearchDestiantion.Size = new System.Drawing.Size(69, 23);
+            this.buttonSearchDestiantion.TabIndex = 2;
+            this.buttonSearchDestiantion.Text = "Search";
+            this.buttonSearchDestiantion.UseVisualStyleBackColor = true;
+            this.buttonSearchDestiantion.Click += new System.EventHandler(this.buttonSearchDestiantion_Click);
+            // 
+            // buttonSearchSource
+            // 
+            this.buttonSearchSource.Location = new System.Drawing.Point(447, 21);
+            this.buttonSearchSource.Name = "buttonSearchSource";
+            this.buttonSearchSource.Size = new System.Drawing.Size(69, 23);
+            this.buttonSearchSource.TabIndex = 1;
+            this.buttonSearchSource.Text = "Search";
+            this.buttonSearchSource.UseVisualStyleBackColor = true;
+            this.buttonSearchSource.Click += new System.EventHandler(this.buttonSearchSource_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Destination Folder\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Source Folder\r\n";
+            // 
+            // textBoxDestination
+            // 
+            this.textBoxDestination.Location = new System.Drawing.Point(152, 55);
+            this.textBoxDestination.Name = "textBoxDestination";
+            this.textBoxDestination.Size = new System.Drawing.Size(289, 22);
+            this.textBoxDestination.TabIndex = 13;
+            this.textBoxDestination.Text = "<Select folder>";
+            // 
+            // textBoxSouce
+            // 
+            this.textBoxSouce.Location = new System.Drawing.Point(152, 21);
+            this.textBoxSouce.Name = "textBoxSouce";
+            this.textBoxSouce.Size = new System.Drawing.Size(289, 22);
+            this.textBoxSouce.TabIndex = 12;
+            this.textBoxSouce.Text = "<Select folder>";
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(447, 93);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(146, 35);
+            this.buttonRun.TabIndex = 3;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 141);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBoxDefault);
+            this.Controls.Add(this.buttonSearchDestiantion);
+            this.Controls.Add(this.buttonSearchSource);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxDestination);
+            this.Controls.Add(this.textBoxSouce);
+            this.Controls.Add(this.buttonRun);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quick backup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBoxDefault;
+        private System.Windows.Forms.Button buttonSearchDestiantion;
+        private System.Windows.Forms.Button buttonSearchSource;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDestination;
+        private System.Windows.Forms.TextBox textBoxSouce;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Quick Backup source code/Quick Backup/Form1.cs
@@ -0,0 +1,289 @@
+﻿//Quick backup
+//this proogram is a simple tool tath allow to copy a source directory in a destination directory
+//and remember a default path.
+//for security reason the program must be installed, run and it can manipulate file only in standard user directory
+
+//date: 08/02/2022
+
+//totaly free license 
+
+//developed by Tombolini Simone
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quick_Backup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+
+                //get the path of the .exe file thath will be runned
+                string EXEPath = "";
+                EXEPath = System.Reflection.Assembly.GetEntryAssembly().Location;
+
+                //generate the path of the setup.bat file
+                string BATPath = EXEPath;
+                BATPath = BATPath.Remove(EXEPath.Length - 17, 17);
+                BATPath = BATPath.Replace('\\', '/');
+                BATPath = BATPath + "/setup.bat";
+
+                //generate the path of the save.txt file
+                string SAVEPath = "";
+                SAVEPath = EXEPath.Remove(EXEPath.Length - 17, 17);
+                SAVEPath = SAVEPath.Replace('\\', '/');
+                SAVEPath = SAVEPath + "/save.txt";
+
+                //unenable the textbox 
+                textBoxSouce.Enabled = false;
+                textBoxDestination.Enabled = false;
+                
+                bool error = false; //error loading file, number lines >=4 
+                //load from the save.txt file the deafult path
+                using (StreamReader reader = new StreamReader(SAVEPath))
+                {
+                    string line; //value of the courrent cycle
+                    int c = 1;//counter
+                   
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        //check if exist a default path
+                        if (c == 1 && line == "flag")
+                        {
+                            checkBoxDefault.Checked = true;
+                        }
+                        //load the default source path
+                        if (c == 2 && checkBoxDefault.Checked == true)
+                        {
+                            textBoxSouce.Text = line;
+                        }
+                        //load the default destination path
+                        if (c == 3 && checkBoxDefault.Checked == true)
+                        {
+                            textBoxDestination.Text = line;
+                        }
+                        //error message
+                        if (c >= 4)
+                        {
+
+                            error = true;
+                        }
+                        c++;
+                    }
+                   
+                }
+                //error 
+                if (error == true)
+                {
+                    //show a a error message
+                    MessageBox.Show(
+                        "Error loading save file",
+                        "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+
+                    //get empty the save.txt file
+                    string[] savelines = { "" };
+
+                    string savePath = SAVEPath;
+
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(savePath, "")))
+                    {
+                        foreach (string Blankline in savelines)
+                            outputFile.WriteLine(Blankline);
+                    }
+                }
+            }
+            catch
+            {
+                //show error message
+                MessageBox.Show(
+                    "Error in program initialization,\n reinstall correctly the program", 
+                    "Error", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+                
+                //close the program
+                if (System.Windows.Forms.Application.MessageLoop)
+                {
+                    System.Windows.Forms.Application.Exit();
+                }
+                else
+                {
+                    System.Environment.Exit(1);
+                }
+            }
+        }
+
+        //button Run
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //confirm the opertation message
+                DialogResult result = MessageBox.Show(
+                    "Do you confirm the operation of the program? \nThe files in the source folder: '\n\n" + 
+                    textBoxSouce.Text + 
+                    "\n\nwill be copied to the destination folder:\n\n" + 
+                    textBoxDestination.Text + 
+                    "\n\nIf the paths are not valid the program will have no effect.",
+                    "Conferma", 
+                    MessageBoxButtons.YesNo, 
+                    MessageBoxIcon.Question);
+                
+                //if user confirm the operation exegute the program, else show cancel message
+                if (result == DialogResult.Yes)
+                {
+                    //get the path of the .exe file thath will be runned
+                    string EXEPath = "";
+                    EXEPath = System.Reflection.Assembly.GetEntryAssembly().Location;
+
+                    //generate the path of the setup.bat file
+                    string BATPath = EXEPath;
+                    BATPath = BATPath.Remove(EXEPath.Length - 17, 17);
+                    BATPath = BATPath.Replace('\\', '/');
+                    BATPath = BATPath + "/setup.bat";
+
+                    //generate the path of the save.txt file
+                    string SAVEPath = "";
+                    SAVEPath = EXEPath.Remove(EXEPath.Length - 17, 17);
+                    SAVEPath = SAVEPath.Replace('\\', '/');
+                    SAVEPath = SAVEPath + "/save.txt";
+
+                    //if this path is the deafult save that in the save.txt file
+                    if (checkBoxDefault.Checked == true)
+                    {
+                        //the save.txt file have 3 line,
+                        //first: control line "flag"
+                        //second: source path
+                        //third: destination path
+                        string[] savelines = { 
+                                "flag", 
+                                textBoxSouce.Text, 
+                                textBoxDestination.Text };
+
+                        //saving the lines
+                        string savePath = SAVEPath;
+
+                        using (StreamWriter outputFile = new StreamWriter(Path.Combine(savePath, "")))
+                        {
+                            foreach (string line in savelines)
+                                outputFile.WriteLine(line);
+                        }
+                    }
+
+                    //creating the robocopy command
+                    string RobocopyString = "";
+
+                    //structure of robocopy:
+                    //"robocopy" source_path destination_path /W:1 /R:2 /XO
+                    // W:1 - specifies to wait 1 second between retries when copying files
+                    // R:2 - specifies the number of retries on failed copies
+                    // XO - This excludes older files/folders if the destination file/folder exists and is the same date
+
+                    RobocopyString = "robocopy " + textBoxSouce.Text + " " + textBoxDestination.Text + " /W:1 /R:2 /XO";
+
+                    string[] lines = { RobocopyString };
+
+
+                    string docPath = BATPath;
+
+                    //writing the setup.bat file whith the command robocopy
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "")))
+                    {
+                        foreach (string line in lines)
+                            outputFile.WriteLine(line);
+                    }
+
+                    //executing the setup.bat file
+                    System.Diagnostics.Process.Start(BATPath);
+
+                    //close the program
+                    if (System.Windows.Forms.Application.MessageLoop)
+                    {
+                        System.Windows.Forms.Application.Exit();
+                    }
+                    else
+                    {
+                        System.Environment.Exit(1);
+                    }
+                }
+                else
+                {
+                    //cancel operation message
+                    MessageBox.Show(
+                        "Operation canceled", 
+                        "Warning", 
+                        MessageBoxButtons.OK, 
+                        MessageBoxIcon.Exclamation);
+                }
+        }
+            catch
+            {
+                //generic error
+                MessageBox.Show(
+                    "Generic error if the problem persists uninstall and reinstall the program", 
+                    "Error", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+            }
+}
+        //search source button
+        private void buttonSearchSource_Click(object sender, EventArgs e)
+        {
+            //get the selected path from usere and put into the source textbox
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxSouce.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        //search destianation button
+        private void buttonSearchDestiantion_Click(object sender, EventArgs e)
+        {
+            //get the selected path from usere and put into the destiantion textbox
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxDestination.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+        //modify source checkbox 
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            //enable/unenable the source textbox
+            if (checkBox2.Checked == true)
+            {
+                textBoxSouce.Enabled = true;
+            }
+            else
+            {
+                textBoxSouce.Enabled = false;
+            }
+        }
+        //modify destination checkbox 
+        private void checkBox3_CheckedChanged_1(object sender, EventArgs e)
+        {
+            //enable/unenable the destination textbox
+            if (checkBox3.Checked == true)
+            {
+                textBoxDestination.Enabled = true;
+            }
+            else
+            {
+                textBoxDestination.Enabled = false;
+            }
+        }
+    }
+}
